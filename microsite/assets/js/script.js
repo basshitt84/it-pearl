0e41ae66b2bea10574591ae5b11a95ba4d231dcf
basshitt84 <157418893+basshitt84@users.noreply.github.com>
2025-03-20 18:19:02 +0000

    slide show workinig

--- microsite/assets/js/script.js
+++ microsite/assets/js/script.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
+
+
+
+
+
 document.getElementById('weatherForm').addEventListener('submit', async function (event) {
   event.preventDefault(); // Stop form from reloading the page
 
